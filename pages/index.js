a433365b3c90fce10777cca09b4f44969ed7cff0
Leonardo Matos <leonardolucas2020@gmail.com>
2024-02-22 21:54:41 -0400

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-function Home(){
-    return(
-        <h1>
-          Amo vocês, Amiguinhos
-        </h1>
-     )
+function Home() {
+  return <h1>Amo vocês, Amiguinhos</h1>;
 }
 
-export default Home
+export default Home;
