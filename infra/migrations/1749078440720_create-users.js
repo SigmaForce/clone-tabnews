be44bcf45cb479f5479b7f308d771d6c5048c1aa
Leonardo Matos <leonardolucas2020@gmail.com>
2025-09-22 20:59:01 -0400

    refactor: add not null to columns and default timezone `created_at` and `updated_at`

--- infra/migrations/1749078440720_create-users.js
+++ infra/migrations/1749078440720_create-users.js
@@ -17,20 +17,22 @@ exports.up = (pgm) => {
       notNull: true,
       unique: true,
     },
-    // why 72 in length? https://security.stackexchange.com/a/39851
+    // why 60 in length? https://npmjs.com/package/bcrypt#hash-info
     password: {
-      type: "varchar(72)",
+      type: "varchar(60)",
       notNull: true,
     },
-    //why timestamp with timezone https://justatheory.com/2012/04/postgres-use-timestamptz/
+    //why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestamptz/
     created_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      default: pgm.func("timezone('utc', now())"),
+      notNull: true,
     },
 
     updated_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      default: pgm.func("timezone('utc', now())"),
+      notNull: true,
     },
   });
 };
